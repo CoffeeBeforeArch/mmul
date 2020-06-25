d0518112c492414e2e70220fbed53ac74860408e
Nick <CoffeeBeforeArch@gmail.com>
2020-06-25 02:46:33 -0500

    add blocked parallel version

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -19,6 +19,45 @@ void blocked_gemm(const double *A, const double *B, double *C, std::size_t N);
 void parallel_gemm(const double *A, const double *b, double *C, std::size_t N,
                    std::size_t start_row, std::size_t end_row);
 
+// Function for naive parallelized GEMM
+void blocked_parallel_gemm(const double *A, const double *b, double *C,
+                           std::size_t N, std::size_t start_row,
+                           std::size_t end_row);
+
+// Serial GEMM benchmark
+static void serial_gemm_bench_power_two(benchmark::State &s) {
+  // Number Dimensions of our matrix
+  std::size_t N = 1 << s.range(0);
+
+  // Create our random number generators
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_real_distribution<double> dist(-10, 10);
+
+  // Create input matrices
+  double *A = new double[N * N];
+  double *B = new double[N * N];
+  double *C = new double[N * N];
+
+  // Initialize them with random values (and C to 0)
+  std::generate(A, A + N * N, [&] { return dist(rng); });
+  std::generate(B, B + N * N, [&] { return dist(rng); });
+  std::generate(C, C + N * N, [&] { return 0; });
+
+  // Main benchmark loop
+  for (auto _ : s) {
+    serial_gemm(A, B, C, N);
+  }
+
+  // Free memory
+  delete[] A;
+  delete[] B;
+  delete[] C;
+}
+BENCHMARK(serial_gemm_bench_power_two)
+    ->DenseRange(8, 10)
+    ->Unit(benchmark::kMillisecond);
+
 // Serial GEMM benchmark
 static void serial_gemm_bench(benchmark::State &s) {
   // Number Dimensions of our matrix
@@ -203,6 +242,62 @@ static void parallel_gemm_bench(benchmark::State &s) {
   free(C);
 }
 BENCHMARK(parallel_gemm_bench)
+    ->DenseRange(8, 10)
+    ->Unit(benchmark::kMillisecond)
+    ->UseRealTime();
+
+// Parallel GEMM benchmark
+static void parallel_blocked_gemm_bench(benchmark::State &s) {
+  // Number Dimensions of our matrix
+  std::size_t N = (1 << s.range(0)) + 16;
+
+  // Create our random number generators
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_real_distribution<double> dist(-10, 10);
+
+  // Create input matrices
+  double *A = static_cast<double *>(aligned_alloc(64, N * N * sizeof(double)));
+  double *B = static_cast<double *>(aligned_alloc(64, N * N * sizeof(double)));
+  double *C = static_cast<double *>(aligned_alloc(64, N * N * sizeof(double)));
+
+  // Initialize them with random values (and C to 0)
+  std::generate(A, A + N * N, [&] { return dist(rng); });
+  std::generate(B, B + N * N, [&] { return dist(rng); });
+  std::generate(C, C + N * N, [&] { return 0; });
+
+  // Set up for launching threads
+  std::size_t num_threads = std::thread::hardware_concurrency();
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+
+  // Calculate values to pass to threads
+  // Assumed to be divisable by num_threads (evenly)
+  std::size_t n_rows = N / num_threads;
+
+  // Main benchmark loop
+  for (auto _ : s) {
+    // Launch threads
+    for (std::size_t i = 0; i < num_threads; i++) {
+      auto start_row = i * n_rows;
+      auto end_row = start_row + n_rows;
+      threads.emplace_back(
+          [&] { blocked_parallel_gemm(A, B, C, N, start_row, end_row); });
+    }
+
+    // Wait for all threads to complete
+    for (auto &t : threads) t.join();
+
+    // Clear the threads each iteration of the benchmark
+    threads.clear();
+  }
+
+  // Free memory
+  free(A);
+  free(B);
+  free(C);
+}
+BENCHMARK(parallel_blocked_gemm_bench)
     ->DenseRange(8, 10)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
