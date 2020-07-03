94aed605cef818513dc45ca22676164ed45f971d
Nick <CoffeeBeforeArch@gmail.com>
2020-07-02 20:38:02 -0700

    add intiial dp implementation

---
+++ src/transpose_vdppd/parallel_transpose_vdppd_gemm.cpp
@@ -0,0 +1,17 @@
+// Implementation of parallel GEMM function
+
+#include <cstddef>
+
+// Parallel implementation
+void parallel_transpose_vdppd_gemm(const double *A, const double *B, double *C,
+                                   std::size_t N, std::size_t start_row,
+                                   std::size_t end_row) {
+  // For each row assigned to this thread...
+  for (std::size_t row = start_row; row < end_row; row++)
+    // For each column...
+    for (std::size_t col = 0; col < N; col++)
+      // For each element in the row-col pair...
+      for (std::size_t idx = 0; idx < N; idx++)
+        // Accumulate the partial results
+        C[row * N + col] += A[row * N + idx] * B[col * N + idx];
+}
