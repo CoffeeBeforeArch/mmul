c45324238c0827f9df230f5e1e0308b237eed7e7
Nick <CoffeeBeforeArch@gmail.com>
2020-07-22 23:25:20 -0400

    add parallel split on columns

--- src/blocked_column_multi_output/benchmark.cpp
+++ src/blocked_column_multi_output/benchmark.cpp
@@ -24,6 +24,13 @@ void blocked_column_multi_output_parallel_gemm(const double *A, const double *B,
                                                std::size_t tid,
                                                std::size_t stride);
 
+// Function prototype for blocked column multi-output parallel GEMM
+void blocked_column_multi_output_parallel_col_gemm(const double *A,
+                                                   const double *B, double *C,
+                                                   std::size_t N,
+                                                   std::size_t tid,
+                                                   std::size_t stride);
+
 // Blocked column multi-output GEMM with aligned memory benchmark
 static void blocked_column_multi_output_aligned_gemm_bench(
     benchmark::State &s) {
@@ -166,6 +173,62 @@ static void parallel_blocked_column_multi_output_gemm_bench(
   free(C);
 }
 BENCHMARK(parallel_blocked_column_multi_output_gemm_bench)
+    ->DenseRange(8, 10)
+    ->Unit(benchmark::kMillisecond)
+    ->UseRealTime();
+
+// Parallel blocked column multi-output GEMM benchmark w/o atomics and
+// collaborative on columns
+static void parallel_blocked_column_multi_output_col_gemm_bench(
+    benchmark::State &s) {
+  // Number Dimensions of our matrix
+  std::size_t N = (1 << s.range(0)) + 16;
+
+  // Create our random number generators
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_real_distribution<double> dist(-10, 10);
+
+  // Create input matrices
+  double *A = static_cast<double *>(aligned_alloc(64, N * N * sizeof(double)));
+  double *B = static_cast<double *>(aligned_alloc(64, N * N * sizeof(double)));
+  double *C = static_cast<double *>(aligned_alloc(64, N * N * sizeof(double)));
+
+  // Initialize them with random values (and C to 0)
+  std::generate(A, A + N * N, [&] { return dist(rng); });
+  std::generate(B, B + N * N, [&] { return dist(rng); });
+  std::generate(C, C + N * N, [&] { return 0; });
+
+  // Set up for launching threads
+  std::size_t num_threads = std::thread::hardware_concurrency();
+  std::size_t stride = num_threads * 16;
+  std::vector<std::thread> threads;
+  threads.reserve(num_threads);
+
+  // Main benchmark loop
+  for (auto _ : s) {
+    // Launch threads
+    for (std::size_t i = 0; i < num_threads - 1; i++) {
+      threads.emplace_back([&] {
+        blocked_column_multi_output_parallel_col_gemm(A, B, C, N, i, stride);
+      });
+    }
+    blocked_column_multi_output_parallel_col_gemm(A, B, C, N, num_threads - 1,
+                                                  stride);
+
+    // Wait for all threads to complete
+    for (auto &t : threads) t.join();
+
+    // Clear the threads each iteration of the benchmark
+    threads.clear();
+  }
+
+  // Free memory
+  free(A);
+  free(B);
+  free(C);
+}
+BENCHMARK(parallel_blocked_column_multi_output_col_gemm_bench)
     ->DenseRange(8, 10)
     ->Unit(benchmark::kMillisecond)
     ->UseRealTime();
