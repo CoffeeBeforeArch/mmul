25084eb8c0a4b8fcaa0fff8abbd0b16b587df8dc
Nick <CoffeeBeforeArch@gmail.com>
2020-11-17 00:51:13 -0600

    update another bench to be SP

--- src/blocked_column/benchmark.cpp
+++ src/blocked_column/benchmark.cpp
@@ -9,14 +9,43 @@
 #include <thread>
 #include <vector>
 
-// Function prototype for blocked column serial MMul
+// Blocked column serial implementation
 void blocked_column_mmul(const float *A, const float *B, float *C,
-                         std::size_t N);
+                         std::size_t N) {
+  // For each chunk of columns
+  for (std::size_t col_chunk = 0; col_chunk < N; col_chunk += 16)
+    // For each row in that chunk of columns...
+    for (std::size_t row = 0; row < N; row++)
+      // For each block of elements in this row of this column chunk
+      // Solve for 16 elements at a time
+      for (std::size_t tile = 0; tile < N; tile += 16)
+        // For each row in the tile
+        for (std::size_t tile_row = 0; tile_row < 16; tile_row++)
+          // Solve for each element in this tile row
+          for (std::size_t idx = 0; idx < 16; idx++)
+            C[row * N + col_chunk + idx] +=
+                A[row * N + tile + tile_row] *
+                B[tile * N + tile_row * N + col_chunk + idx];
+}
 
-// Fucntion prototype for blocked column parallel MMul
+// Blocked serial implementation
 void blocked_column_parallel_mmul(const float *A, const float *B, float *C,
                                   std::size_t N, std::size_t start_col,
-                                  std::size_t end_col);
+                                  std::size_t end_col) {
+  for (auto col_chunk = start_col; col_chunk < end_col; col_chunk += 16)
+    // For each row in that chunk of columns...
+    for (std::size_t row = 0; row < N; row++)
+      // For each block of elements in this row of this column chunk
+      // Solve for 16 elements at a time
+      for (std::size_t tile = 0; tile < N; tile += 16)
+        // For each row in the tile
+        for (std::size_t tile_row = 0; tile_row < 16; tile_row++)
+          // Solve for each element in this tile row
+          for (std::size_t idx = 0; idx < 16; idx++)
+            C[row * N + col_chunk + idx] +=
+                A[row * N + tile + tile_row] *
+                B[tile * N + tile_row * N + col_chunk + idx];
+}
 
 // Blocked column MMul with aligned memory benchmark
 static void blocked_column_aligned_mmul_bench(benchmark::State &s) {
@@ -36,7 +65,7 @@ static void blocked_column_aligned_mmul_bench(benchmark::State &s) {
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
   std::generate(B, B + N * N, [&] { return dist(rng); });
-  std::generate(C, C + N * N, [&] { return 0; });
+  std::generate(C, C + N * N, [&] { return 0.0f; });
 
   // Main benchmark loop
   for (auto _ : s) {
@@ -72,7 +101,7 @@ static void parallel_blocked_column_mmul_bench(benchmark::State &s) {
   // Initialize them with random values (and C to 0)
   std::generate(A, A + N * N, [&] { return dist(rng); });
   std::generate(B, B + N * N, [&] { return dist(rng); });
-  std::generate(C, C + N * N, [&] { return 0; });
+  std::generate(C, C + N * N, [&] { return 0.0f; });
 
   // Set up for launching threads
   std::size_t num_threads = std::thread::hardware_concurrency();
@@ -87,14 +116,13 @@ static void parallel_blocked_column_mmul_bench(benchmark::State &s) {
   for (auto _ : s) {
     // Launch threads
     std::size_t start_col = 0;
-    for (std::size_t i = 0; i < num_threads - 1; i++) {
+    for (std::size_t i = 0; i < num_threads; i++) {
       auto end_col = start_col + n_cols;
       threads.emplace_back([&] {
         blocked_column_parallel_mmul(A, B, C, N, start_col, end_col);
       });
       start_col += n_cols;
     }
-    blocked_column_parallel_mmul(A, B, C, N, start_col, start_col + n_cols);
 
     // Wait for all threads to complete
     for (auto &t : threads) t.join();
